84c35e46c011edcb066c72651b25607206b5ded2
mbrodziak <matousb96@gmail.com>
2018-10-17 23:36:31 +0200

    Faza pierwsza - piąty commit

--- app/src/main/java/com/example/mateusz/lamimozgi/GameChoiceActivity.java
+++ app/src/main/java/com/example/mateusz/lamimozgi/GameChoiceActivity.java
@@ -1,6 +1,14 @@
 package com.example.mateusz.lamimozgi;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class GameChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_choice);
+    }
+
 }
--- app/src/main/java/com/example/mateusz/lamimozgi/WelcomeActivity.java
+++ app/src/main/java/com/example/mateusz/lamimozgi/WelcomeActivity.java
@@ -1,8 +1,34 @@
 package com.example.mateusz.lamimozgi;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        final Thread thread = new Thread(){
+            public void run() {
+                try {
+                    sleep(3500);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        finish();
+    }
 
 
 }
